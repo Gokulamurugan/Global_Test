95ef8bbf218a9834fc324fb31dfcc2f4f7c878be
Gokulamurugan <gokulamurugan1@gmail.com>
2021-11-01 21:03:47 +0000

    Eclispe

--- src/test/scala/Simulations/AddPauseTime.scala
+++ src/test/scala/Simulations/AddPauseTime.scala
@@ -14,7 +14,7 @@ class AddPauseTime extends Simulation {
   val scn = scenario("VideoGame 3 API with Pause Time")
     .exec(http("Get all games")
       .get("videogames"))
-    .pause(duration = 10)
+    .pause(duration = 11)
 
     .exec(http("Get specific game")
       .get("videogames/1"))
